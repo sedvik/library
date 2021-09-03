3a33bac7317eea7c321fef06f8539ee79e5a9edb
Skyler Edvik <edvik.skyler@gmail.com>
2021-09-03 17:09:35 -0600

    Add example book card html to index.html

--- library.js
+++ library.js
@@ -1,5 +1,24 @@
 // myLibrary - an array containing book objects
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R Tolkien",
+        pages: 304,
+        read: true
+    },
+    {
+        title: "The Name of the Wind",
+        author: "Patrick Rothfuss",
+        pages: 662,
+        read: true
+    },
+    {
+        title: "The Shining",
+        author: "Stephen King",
+        pages: 659,
+        read: false
+    }
+];
 
 // Book constructor
 function Book(title, author, pages, read) {
@@ -12,6 +31,21 @@ function Book(title, author, pages, read) {
 // addBookToLibrary function - adds a single book to the myLibrary array
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+// createBookElement - generates a book div element from a book object instance
+function createBookElement(book) {
+    const bookElement = document.createElement('div');
+
+}
+
+// renderBooks function - Loops through myLibrary array and adds each book to the DOM
+function renderBooks() {
+    const bookContainer = document.querySelector('#book-container');
+    myLibrary.forEach(function(book) {
+        const bookElement = createBookElement(book);
+        bookContainer.appendChild(bookElement);
+    });
 }
 
 /* Event Handler functions */
