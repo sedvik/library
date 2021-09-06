0f8f300e4bdd6e6627274bd7798cc215bef5abea
Skyler Edvik <edvik.skyler@gmail.com>
2021-09-06 02:09:23 -0600

    Complete createBookElement function

--- library.js
+++ library.js
@@ -35,8 +35,100 @@ function addBookToLibrary(book) {
 
 // createBookElement - generates a book div element from a book object instance
 function createBookElement(book) {
+    // Determine what the index of the new book will be based on myLibrary length
+    const index = myLibrary.length;
+    
+    // Create bookElement container div
     const bookElement = document.createElement('div');
+    bookElement.classList.add('book-card');
 
+    // Create book title
+    const bookTitle = document.createElement('h3');
+    bookTitle.textContent = book.title;
+
+    // Create Author card entry
+    const authorDiv = document.createElement('div');
+    authorDiv.classList.add('card-entry');
+
+    const authorH4 = document.createElement('h4');
+    authorH4.textContent = 'Author:';
+
+    const authorP = document.createElement('p');
+    authorP.textContent = book.author;
+
+    authorDiv.appendChild(authorH4);
+    authorDiv.appendChild(authorP);
+
+    // Create Pages card entry
+    const pagesDiv = document.createElement('div');
+    pagesDiv.classList.add('card-entry');
+
+    const pagesH4 = document.createElement('h4');
+    pagesH4.textContent = 'Pages:';
+
+    const pagesP = document.createElement('p');
+    pagesP.textContent = book.pages;
+
+    pagesDiv.appendChild(pagesH4);
+    pagesDiv.appendChild(pagesP);
+
+    // Create "read?" card entry
+    const readDiv = document.createElement('div');
+    readDiv.classList.add('card-entry');
+
+    const readH4 = document.createElement('h4');
+    readH4.textContent = 'Read?';
+
+    // TODO - Create a separate function for generating this div
+    const bookReadDiv = document.createElement('div');
+    bookReadDiv.classList.add('book-read');
+
+    const yesInput = document.createElement('input');
+    yesInput.id = `${index}-yes`;
+    yesInput.setAttribute('type', 'radio');
+    yesInput.setAttribute('name', 'read');
+    yesInput.setAttribute('value', 'yes');
+
+    const yesLabel = document.createElement('label');
+    yesLabel.setAttribute('for', `${index}-yes`);
+    yesLabel.textContent = 'Yes';
+
+    const noInput = document.createElement('input');
+    noInput.id = `${index}-no`;
+    noInput.setAttribute('type', 'radio');
+    noInput.setAttribute('name', 'read');
+    noInput.setAttribute('value', 'no');
+
+    const noLabel = document.createElement('label');
+    noLabel.setAttribute('for', `${index}-no`);
+    noLabel.textContent = 'No';
+
+    bookReadDiv.appendChild(yesInput);
+    bookReadDiv.appendChild(yesLabel);
+    bookReadDiv.appendChild(noInput);
+    bookReadDiv.appendChild(noLabel);
+
+    readDiv.appendChild(readH4);
+    readDiv.appendChild(bookReadDiv);
+
+    // Create remove book button card entry
+    const removeBookDiv = document.createElement('div');
+    removeBookDiv.classList.add('card-entry');
+
+    const removeBookBtn = document.createElement('button');
+    removeBookBtn.classList.add('remove-book');
+    removeBookBtn.textContent = 'Remove';
+
+    removeBookDiv.appendChild(removeBookBtn);
+
+    // Add all created elements to book-card
+    bookElement.appendChild(bookTitle);
+    bookElement.appendChild(authorDiv);
+    bookElement.appendChild(pagesDiv);
+    bookElement.appendChild(readDiv);
+    bookElement.appendChild(removeBookDiv);
+
+    return bookElement;
 }
 
 // renderBooks function - Loops through myLibrary array and adds each book to the DOM
@@ -69,3 +161,6 @@ function createBook(e) {
 }
 
 /* Add event listeners to page buttons */
+
+// Initial call of renderBooks function
+renderBooks();
