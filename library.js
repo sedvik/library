36b9172cf93fb14d565bedd46673d26c9932e066
Skyler Edvik <edvik.skyler@gmail.com>
2021-09-07 15:06:30 -0600

    Complete createBook and toggleForm event handlers

--- library.js
+++ library.js
@@ -28,9 +28,10 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-// addBookToLibrary function - adds a single book to the myLibrary array
+// addBookToLibrary function - adds a single book to the myLibrary array and re-renders the books
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    renderBooks();
 }
 
 // createBookElement - generates a book div element from a book object instance
@@ -89,9 +90,8 @@ function createBookElement(book) {
     yesInput.setAttribute('name', `read-${index}`);
     yesInput.setAttribute('value', 'yes');
     // If the book has been read, set this radio button as checked
-    if (book.read) {
-        yesInput.checked = 'checked';
-    }
+    if (book.read) yesInput.checked = 'checked';
+   
     
 
     const yesLabel = document.createElement('label');
@@ -104,9 +104,8 @@ function createBookElement(book) {
     noInput.setAttribute('name', `read-${index}`);
     noInput.setAttribute('value', 'no');
     // If the book has not been read, set this radio button as checked
-    if (!book.read) {
-        noInput.checked = 'checked';
-    }
+    if (!book.read) noInput.checked = 'checked';
+
 
     const noLabel = document.createElement('label');
     noLabel.setAttribute('for', `${index}-no`);
@@ -142,6 +141,9 @@ function createBookElement(book) {
 
 // renderBooks function - Loops through myLibrary array and adds each book to the DOM
 function renderBooks() {
+    // Clear any initial books from #book-container
+    document.querySelector('#book-container').textContent = '';
+    
     const bookContainer = document.querySelector('#book-container');
     myLibrary.forEach(function(book) {
         const bookElement = createBookElement(book);
@@ -149,27 +151,65 @@ function renderBooks() {
     });
 }
 
+// isValidForm function - checks to ensure new book form inputs are valid
+function isValidForm() {
+
+}
+
 /* Event Handler functions */
 
 // createBook function - creates a book based on user form input
 function createBook(e) {
-    // Prevent default form submission behavior
+    // Prevent default form submission behavior to prevent page reload
     e.preventDefault();
+
+    // Initialize form attribute object
+    const formAtr = {};
     
     // Extract form values
+    const textInputArr = Array.from(document.querySelectorAll('#new-book-form input[type="text"'));
+    textInputArr.forEach(input => {
+        formAtr[input.name] = input.value;
+    });
+    
+    const formPages = document.querySelector('#num-pages');
+    formAtr.pages = formPages.value;
+
+    const radioInput = document.querySelector('#new-book-form input[type="radio"]:checked');
+    formAtr.read = radioInput.value === 'yes';
 
     // Create book instance
-    const book = {};
+    const book = new Book(formAtr.title, formAtr.author, formAtr.pages, formAtr.read);
 
     // add the book to the library
     addBookToLibrary(book);
 
     // Clear all form fields
+    document.querySelector('#new-book-form').reset();
 
     // Hide the form
+    document.querySelector('#new-book-form').style.display = 'none';
+}
+
+// toggleForm function - toggles the visibility of the new book form
+function toggleForm(e) {
+    // Toggle the visibility of the new book form and the display text of the new-book-btn
+    const newBookForm = document.querySelector('#new-book-form');
+    if (e.target.textContent === 'Add New Book') {
+        newBookForm.style.display = 'block';
+        e.target.textContent = 'Hide New Book Form';
+    } else {
+        newBookForm.style.display = 'none';
+        e.target.textContent = 'Add New Book';
+    }
 }
 
 /* Add event listeners to page buttons */
+const addBookBtn = document.querySelector('#add-book');
+addBookBtn.addEventListener('click', createBook);
+
+const addNewBookBtn = document.querySelector('#new-book-btn');
+addNewBookBtn.addEventListener('click', toggleForm);
 
 // Initial call of renderBooks function
 renderBooks();
